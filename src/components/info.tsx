de683ab542f0a62f8bc3ed58d9def00900f31f35
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2024-12-06 19:01:41 -0300

    Feat: Componente de informações

---
+++ src/components/info.tsx
@@ -0,0 +1,42 @@
+function Infos() {
+  return (
+    <>
+      <article className="infos">
+        <label>
+          Nome do jogador:
+          <input name="jogador" className="inputText" type="text" />
+        </label>
+        <label>
+          Nome do personagem:
+          <input name="personagem" className="inputText" type="text" />
+        </label>
+        <label>
+          Idade:
+          <input name="idade" className="inputText" type="text" />
+        </label>
+        <label>
+          NEX:
+          <input name="nex" className="inputText" type="text" />
+        </label>
+        <label>
+          Classe:
+          <input name="classe" className="inputText" type="text" />
+        </label>
+        <label>
+          Trilha:
+          <input name="trilha" className="inputText" type="text" />
+        </label>
+        <label>
+          Origem:
+          <input name="origem" className="inputText" type="text" />
+        </label>
+        <label>
+          Patente:
+          <input name="patente" className="inputText" type="text" />
+        </label>
+      </article>
+    </>
+  )
+}
+
+export default Infos;
