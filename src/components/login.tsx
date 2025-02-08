bb3a5807a14e351cb36d662643e3b467e9515277
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2025-02-08 02:19:18 -0300

    Feat + Style: Estilizada página de login + adicionado botão de lembrar-me

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -29,35 +29,48 @@ function Login () {
   return (
     <>
       <main>
-        <label htmlFor='usuario'>
-          Usuário ou email:
-          <input
-            type='text'
-            name='usuario'
-            className='usuario'
-            value={state.usuario}
-            onChange={handleChange}
-            placeholder='Insira seu usuário ou email'
-          />
-        </label>
-        <label htmlFor='senha'>
-          Senha:
-          <input
-            type='text'
-            name='senha'
-            className='senha'
-            value={state.senha}
-            onChange={handleChange}
-            placeholder='Insira sua senha'
-          />
-        </label>
-        <button className='entrar' onClick={entrar}>Entrar</button>
-        <button className='esqueceuSenha'>
-          <Link to='/recuperar-senha'>Esqueci minha senha</Link>
-        </button>
-        <button className='criarConta'>
-          <Link to='/criar-conta'>Criar uma conta</Link>
-        </button>
+        <h1 className='titulo'>Fichas Ordem</h1>
+        <div className='textoCriarConta'>
+          Não possui um cadastro?
+          <button className='criarConta'>
+            <Link to='/criar-conta' id='botaoCriarConta'>Registre-se</Link>
+          </button>
+        </div>
+        <div className='cardLogin'>
+          <h1 id='bemVindo'>Bem-vindo!</h1>
+          <label htmlFor='usuario' className='inputInfo'>
+            Usuário ou email:
+            <input
+              type='text'
+              name='usuario'
+              className='usuario'
+              value={state.usuario}
+              onChange={handleChange}
+              placeholder='Insira seu usuário ou email'
+            />
+          </label>
+          <label htmlFor='senha' className='inputInfo'>
+            Senha:
+            <input
+              type='text'
+              name='senha'
+              className='senha'
+              value={state.senha}
+              onChange={handleChange}
+              placeholder='Insira sua senha'
+            />
+          </label>
+          <button className='esqueceuSenha'>
+            <Link to='/recuperar-senha' id='botaoEsqueceuSenha'>Esqueci minha senha</Link>
+          </button>
+          <div className='checkbox'>
+            <input type='checkbox' className='lembrar' id='lembrar'/>
+            <label className='lembrarInfo' htmlFor='lembrar'>
+              Lembrar-me
+            </label>
+          </div>
+          <button className='entrar' onClick={entrar}>Entrar</button>
+        </div>
       </main>
     </>
   )
