1fff6efc65e13e0937ed961f075113477b1cb082
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2025-02-11 02:25:12 -0300

    Feat: Carrossel de campanhas criado

---
+++ src/mocks/mockCampanhas.tsx
@@ -0,0 +1,40 @@
+const CAMPANHAS = [
+  {
+    id: 1,
+    nome: 'Vale da lua',
+    imagem: '../public/placeholder.png'
+  },
+  {
+    id: 2,
+    nome: 'Espinhos da aurora escarlate',
+    imagem: '../public/placeholder.png'
+  },
+  {
+    id: 3,
+    nome: 'Fantasminha Camarada',
+    imagem: '../public/placeholder.png'
+  },
+  {
+    id: 4,
+    nome: 'Terror nas 7 cidades',
+    imagem: '../public/placeholder.png'
+  },
+  {
+    id: 5,
+    nome: 'Plantação Assombrada',
+    imagem: '../public/placeholder.png'
+  },
+  {
+    id: 6,
+    nome: 'Hospital infinito',
+    imagem: '../public/placeholder.png'
+  },
+  {
+    id: 7,
+    nome: 'Debaixo da superfície',
+    imagem: '../public/placeholder.png'
+  }
+  
+]
+
+export default CAMPANHAS;
--- src/pages/campanhas.tsx
+++ src/pages/campanhas.tsx
@@ -1,18 +1,70 @@
 import '../css/campanhas.css';
+import { useEffect, useState, useRef } from 'react';
+import { motion } from 'framer-motion';
+import CAMPANHAS from '../mocks/mockCampanhas'; // APENAS DURANTE O DESENVOLVIMENTO
 
 function Campanhas () {
+  interface Campanha {
+    id: number;
+    nome: string;
+    imagem: string;
+  };
+
+  const [campanhas, setCampanhas] = useState<Campanha[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [widthCarrossel, setWidthCarrossel] = useState(0)
+
+  const carrossel = useRef<HTMLDivElement>(null);
+
+  function getCampanhas () {
+    setLoading(true);
+    setTimeout(() => {
+      setCampanhas(CAMPANHAS);
+      setLoading(false);
+    }, 1000)
+  }
+
+  useEffect(() => {
+    getCampanhas();
+    if (carrossel.current){
+      const scrollWidth = carrossel.current.scrollWidth;
+      const offsetWidth = carrossel.current.offsetWidth;
+      setWidthCarrossel(scrollWidth - offsetWidth);
+    }
+  }, [])
+
   return (
     <>
       <main>
-        <article className='cardCampanha'>
-          <img
-            src='..//public/plAceholder.png'
-            alt='Placeholder para imagem da campanha'
-            className='fotoCampanha'
-          />
-          <h1 className='tituloCampanha'> NOME DA CAMPANHA</h1>
-          <button className='acessarCampanha'> Acessar </button>
-        </article>
+        <motion.div
+          className='carrosselCampanha' ref={carrossel}>
+          { loading ? (
+            <h1>CARREGANDO</h1>
+          ) : (
+            <motion.div className='carrosselInterno' drag='x' dragConstraints={{ right: 0, left: -widthCarrossel }}>
+              {campanhas.map((campanha) => (
+                <motion.article className='cardCampanha' key={campanha.id}>
+                  <img
+                    src={campanha.imagem}
+                    alt='Placeholder para imagem da campanha'
+                    className='fotoCampanha'
+                  />
+                  <h1 className='tituloCampanha'>{campanha.nome}</h1>
+                  <button className='acessarCampanha'> Acessar </button>
+                </motion.article>
+                ))}
+                <motion.article className='cardCampanha'>
+                  <img
+                    src='../public/placeholder.png'
+                    alt='Placeholder para imagem de criar campanha'
+                    className='fotoCampanha'
+                  />
+                  <h1 className='tituloCampanha'>Nova campanha</h1>
+                  <button className='acessarCampanha'> Criar </button>
+                </motion.article>
+              </motion.div>
+          )}
+        </motion.div>
       </main>
     </>
   )
