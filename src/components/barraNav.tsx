be59bb9bffa3c11f4ec923df278a6545f3c713ee
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2025-04-02 01:51:53 -0300

    Feat: ajusta botões de navegação e adiciona titulo

--- src/components/barraNav.tsx
+++ src/components/barraNav.tsx
@@ -5,10 +5,10 @@ function BarraNav() {
   return (
     <>
       <nav className='navBar'>
+        <h1 className='title'>Ordem Paranormal</h1>
         <NavLink to="/" className='buttonNav'>Sair</NavLink>
         <NavLink to="/perfil" className='buttonNav'>Perfil</NavLink>
         <NavLink to="/campanhas" className='buttonNav'>Campanhas</NavLink>
-        <NavLink to="/personagem" className='buttonNav'>Personagens</NavLink>
       </nav>  
     </>
   )
